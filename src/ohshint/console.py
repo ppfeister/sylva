b44b7f81472b359dbb3d05a5200995dc7ccba9f8
Paul Pfeister <code@pfeister.dev>
2024-07-12 17:34:42 -0400

    feat: add version flag

--- src/ohshint/console.py
+++ src/ohshint/console.py
@@ -1,10 +1,10 @@
 from argparse import ArgumentParser
 
-from .__init__ import __short_name__
+from .__init__ import __short_name__, __long_name__, __version__
 from .config import config, InteractiveConfig
 
 def interactive():
-    parser = ArgumentParser(description=f'{__short_name__}')
+    parser = ArgumentParser(description=f'{__long_name__}')
     subparsers = parser.add_subparsers(dest='command')
 
     parser_search = subparsers.add_parser('find', help='Find an identity')
@@ -13,6 +13,14 @@ def interactive():
 
     parser_config = subparsers.add_parser('config', help=f'Edit {__short_name__}\'s config')
     parser_config.add_argument('--edit', action=InteractiveConfig, help='Edit the config interactively')
+
+    parser.add_argument(
+        '--version',
+        '-v',
+        action='version',
+        version=f'{__short_name__} {__version__}',
+        help='Show the version and exit'
+    )
 
     args = parser.parse_args()
     if args.command:
