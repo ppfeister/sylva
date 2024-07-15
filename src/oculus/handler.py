4893a128033249341a5c7b94f3e5dbd2e1fea516
Paul Pfeister <code@pfeister.dev>
2024-07-14 21:00:29 -0400

    chore: simplify handler

--- src/oculus/handler.py
+++ src/oculus/handler.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from .collector import Collector
 from .helpers.helpers import RequestError
 from .integrations import (
@@ -13,24 +15,15 @@ from .modules import (
 class Handler:
     def __init__(self):
         self.collector:Collector = Collector()
+        self.runners:List = [
+            proxynova.ProxyNova(collector=self.collector),
+            endato.Endato(collector=self.collector),
+            #intelx.IntelX(collector=self.collector),
+            pgp_module.PGPModule(collector=self.collector),
+        ]
     def search_all(self, query:str):
-        try:
-            _proxynova = proxynova.ProxyNova(collector=self.collector)
-            _proxynova.search(query=query)
-        except RequestError:
-            pass
-        try:
-            _endato = endato.Endato(collector=self.collector)
-            _endato.search(query=query)
-        except RequestError:
-            pass
-        #try:
-        #    _intelx = intelx.IntelX(collector=self.collector)
-        #    _intelx.search(query=query)
-        #except RequestError:
-        #    pass
-        try:
-            _pgp = pgp_module.PGPModule(collector=self.collector)
-            _pgp.search(query=query)
-        except RequestError:
-            pass
+        for runner in self.runners:
+            try:
+                runner.search(query=query)
+            except RequestError:
+                pass
