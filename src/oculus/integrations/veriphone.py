f916aacf49adfc75b93c7e353855f5a342091b80
Paul Pfeister <code@pfeister.dev>
2024-07-15 04:25:32 -0400

    chore: cleanup dead code

--- src/oculus/integrations/veriphone.py
+++ src/oculus/integrations/veriphone.py
@@ -45,19 +45,3 @@ class Veriphone:
         ]
         self.collector.insert(pd.DataFrame(raw_rows))
         return pd.DataFrame(raw_rows)
-
-        print(json_data)
-
-        sanitized_query = requests.utils.requote_uri(query)
-        response = requests.get(self.__api_url.format(QUERY=sanitized_query, START=start, END=end))
-        if response.status_code != 200:
-            raise RequestError(f'Failed to get results from ProxyNova. Status code: {response.status_code}')
-        json_data:dict = json.loads(response.text)
-        rows = [line.split(':') if ':' in line else [line, None] for line in json_data['lines']] # ugly but functional
-        new_data = pd.DataFrame(rows[:1], columns=['email', 'password'])
-        new_data['query'] = query
-        new_data['source_name'] = self.source_name
-        new_data['spider_recommended'] = config['Target Options']['proxycheck-spider-out']
-        self.collector.insert(new_data)
-        return new_data
-    #def accepts(self, query:str) -> bool:
